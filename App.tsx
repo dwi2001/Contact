d3e9191ceb5dbd78de2e8942ced2cf1ba1572103
dwi2001 <dwi.nugraha@admo.astra.co.id>
2024-06-16 10:34:30 +0700

    splash screen android

--- App.tsx
+++ App.tsx
@@ -5,10 +5,13 @@ import HomeNavigation from './src/Navigation/HomeNavigation';
 import {Provider} from 'react-redux';
 import store from './src/Redux/Store';
 import SplashScreen from 'react-native-splash-screen';
+import {Platform} from 'react-native';
 
 function App(): React.JSX.Element {
   useEffect(() => {
-    SplashScreen.hide();
+    {
+      Platform.OS === 'ios' ? SplashScreen.hide() : null;
+    }
   }, []);
 
   return (
